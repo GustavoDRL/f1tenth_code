93b7a5497925a80cb20c094cede26fbfadcfb92c
Disney <gustavo.rio.lima@br.ey.com>
2025-02-05 23:18:10 -0300

    4

--- build/my_robot_mapper/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /sim_ws/src/my_robot_mapper
-# Build directory: /sim_ws/build/my_robot_mapper
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
